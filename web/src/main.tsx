e908306f4aa929e14afc6861726afcc3487b15c4
Samuel B. <samuel@futurejobspro.com>
2026-05-24 12:59:47 -0600

    Test: Check if React renders at all

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';   // comment out for testing
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div style={{ color: 'white', fontSize: 40, textAlign: 'center', marginTop: 100 }}>
+    🚀 React is alive!
+  </div>,
 );
